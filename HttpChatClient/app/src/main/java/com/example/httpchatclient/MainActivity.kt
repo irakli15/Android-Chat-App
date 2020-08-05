17b2f924ae36592e6a00a88ab83f38ca80fb43e3
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2020-08-06 01:58:01 +0400

    chat history page partially done

--- HttpChatClient/app/src/main/java/com/example/httpchatclient/MainActivity.kt
+++ HttpChatClient/app/src/main/java/com/example/httpchatclient/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.httpchatclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.start_page)
-//        val toolbar = findViewById<CollapsingToolbarLayout>(R.id.toolbar)
-//        toolbar.title = "title"
+        setContentView(R.layout.activity_main)
+//        collapsing_toolbar.isTitleEnabled = false
+//        collapsing_toolbar.title = "title"
     }
 }
