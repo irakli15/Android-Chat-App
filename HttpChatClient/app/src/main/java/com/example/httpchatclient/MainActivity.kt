6ee28efd2304456509f7f9eee6e5e3faecb58a42
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2020-08-05 23:23:17 +0400

    start page done

--- HttpChatClient/app/src/main/java/com/example/httpchatclient/MainActivity.kt
+++ HttpChatClient/app/src/main/java/com/example/httpchatclient/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.httpchatclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.start_page)
+//        val toolbar = findViewById<CollapsingToolbarLayout>(R.id.toolbar)
+//        toolbar.title = "title"
     }
 }
